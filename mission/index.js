8fca80d88719d9640ce93371fca0fd4623015c9b
BingChiLing202 <zijiec214@gmail.com>
2026-09-25 13:47:15 -0600

    q

--- mission/index.js
+++ mission/index.js
@@ -1,15 +1,25 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+let body = document.querySelector('body');
+let classHey = document.querySelector('.hey')
+
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        // code for changes to colors and logo
+        body.style.backgroundColor = 'gray';
+        body.style.color = 'white';
+        logo.setAttribute('src','byui-logo-white.png')
+        classHey.style.color = 'lightblue'
     } else {
-        // code for changes to colors and logo
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black'
+        logo.setAttribute('src','byui-logo-blue.webp')
+        classHey.style.color = '#3C99DC'
+
     }
 }           
                     
